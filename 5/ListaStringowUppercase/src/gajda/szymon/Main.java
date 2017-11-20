f9bcba7ce9e63196f4ddf0d9c3a3e6d6cc98054e
Mrucznik <mrucznix@gmail.com>
2017-11-20 23:01:32 +0100

    Zadanie 2

--- 5/ListaStringowUppercase/src/gajda/szymon/Main.java
+++ 5/ListaStringowUppercase/src/gajda/szymon/Main.java
@@ -21,7 +21,7 @@ public class Main {
         return list;
     }
 
-    public static ArrayList<String> sposob1(ArrayList<String> list)
+    private static ArrayList<String> sposob1(ArrayList<String> list)
     {
         ListIterator<String> i = list.listIterator();
         while(i.hasNext())
@@ -32,7 +32,7 @@ public class Main {
         return list;
     }
 
-    public static ArrayList<String> sposob2(ArrayList<String> list)
+    private static ArrayList<String> sposob2(ArrayList<String> list)
     {
         for(int i=0; i<list.size(); i++)
         {
@@ -41,7 +41,7 @@ public class Main {
         return list;
     }
 
-    public static ArrayList<String> sposob3(ArrayList<String> list)
+    private static ArrayList<String> sposob3(ArrayList<String> list)
     {
         list.replaceAll(String::toUpperCase);
         return list;
